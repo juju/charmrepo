63f8885ea614b7e9cf78fe49d3e4564223cf6199
Ian Booth <ian.booth@canonical.com>
2015-11-26 13:43:46 +1000

    Add new multi-series test

--- charmstore_test.go
+++ charmstore_test.go
@@ -91,8 +91,8 @@ func (s *charmStoreBaseSuite) startServer(c *gc.C) {
 	})
 }
 
-// addCharm uploads a charm to the testing charm store, and returns the
-// resulting charm and charm URL.
+// addCharm uploads a charm a promulgated revision to the testing charm store,
+// and returns the resulting charm and charm URL.
 func (s *charmStoreBaseSuite) addCharm(c *gc.C, urlStr, name string) (charm.Charm, *charm.URL) {
 	id := charm.MustParseURL(urlStr)
 	promulgatedRevision := -1
@@ -111,6 +111,26 @@ func (s *charmStoreBaseSuite) addCharm(c *gc.C, urlStr, name string) (charm.Char
 	c.Assert(err, jc.ErrorIsNil)
 
 	return ch, id
+}
+
+// addCharmNoRevision uploads a charm to the testing charm store, and returns the
+// resulting charm and charm URL.
+func (s *charmStoreBaseSuite) addCharmNoRevision(c *gc.C, urlStr, name string) (charm.Charm, *charm.URL) {
+	id := charm.MustParseURL(urlStr)
+	if id.User == "" {
+		id.User = "who"
+	}
+	ch := TestCharms.CharmArchive(c.MkDir(), name)
+
+	// Upload the charm.
+	url, err := s.client.UploadCharm(id, ch)
+	c.Assert(err, gc.IsNil)
+
+	// Allow read permissions to everyone.
+	err = s.client.Put("/"+url.Path()+"/meta/perm/read", []string{params.Everyone})
+	c.Assert(err, jc.ErrorIsNil)
+
+	return ch, url
 }
 
 // addBundle uploads a bundle to the testing charm store, and returns the
@@ -502,6 +522,7 @@ func (s *charmStoreRepoSuite) TestResolve(c *gc.C) {
 	s.addCharm(c, "~who/trusty/mysql-0", "mysql")
 	s.addCharm(c, "~who/precise/wordpress-2", "wordpress")
 	s.addCharm(c, "~dalek/utopic/riak-42", "riak")
+	s.addCharmNoRevision(c, "multi-series", "multi-series")
 	s.addCharm(c, "utopic/mysql-47", "mysql")
 
 	// Define the tests to be run.
@@ -542,6 +563,10 @@ func (s *charmStoreRepoSuite) TestResolve(c *gc.C) {
 		url:             "cs:utopic/mysql-47",
 		supportedSeries: []string{"utopic"},
 	}, {
+		id:              "~who/multi-series",
+		url:             "cs:~who/multi-series-0",
+		supportedSeries: []string{"trusty", "precise", "quantal"},
+	}, {
 		id:  "~dalek/utopic/riak-100",
 		err: `cannot resolve URL "cs:~dalek/utopic/riak-100": charm not found`,
 	}, {
@@ -557,13 +582,13 @@ func (s *charmStoreRepoSuite) TestResolve(c *gc.C) {
 		c.Logf("test %d: %s", i, test.id)
 		ref, supportedSeries, err := s.repo.Resolve(charm.MustParseURL(test.id))
 		if test.err != "" {
-			c.Assert(err.Error(), gc.Equals, test.err)
-			c.Assert(ref, gc.IsNil)
+			c.Check(err.Error(), gc.Equals, test.err)
+			c.Check(ref, gc.IsNil)
 			continue
 		}
 		c.Assert(err, jc.ErrorIsNil)
-		c.Assert(ref, jc.DeepEquals, charm.MustParseURL(test.url))
-		c.Assert(supportedSeries, jc.DeepEquals, test.supportedSeries)
+		c.Check(ref, jc.DeepEquals, charm.MustParseURL(test.url))
+		c.Check(supportedSeries, jc.SameContents, test.supportedSeries)
 	}
 }
 
