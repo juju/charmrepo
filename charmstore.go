87b8c25c5878f95f9499906a48a4c9833db65811
Eric Snow <ericsnowcurrently@gmail.com>
2016-02-18 09:01:47 -0700

    Add the charmrepo.CharmStoreClient interface.

--- charmstore.go
+++ charmstore.go
@@ -24,10 +24,47 @@ import (
 // CacheDir stores the charm cache directory path.
 var CacheDir string
 
+// CharmStoreClient exposes the functionality of csclient.Client needed here.
+type CharmStoreClient interface {
+	// GetArchive retrieves the archive for the given charm or bundle,
+	// returning a reader its data can be read from, the fully qualified
+	// id of the corresponding entity, the SHA384 hash of the data and
+	// its size.
+	GetArchive(*charm.URL) (r io.ReadCloser, eid *charm.URL, hash string, size int64, err error)
+
+	// Get makes a GET request to the given path in the charm store (not
+	// including the host name or version prefix but including a leading /),
+	// parsing the result as JSON into the given result value, which should
+	// be a pointer to the expected data, but may be nil if no result is
+	// desired.
+	Get(path string, result interface{}) error
+
+	// Meta fetches metadata on the charm or bundle with the
+	// given id. The result value provides a value
+	// to be filled in with the result, which must be
+	// a pointer to a struct containing members corresponding
+	// to possible metadata include parameters
+	// (see https://github.com/juju/charmstore/blob/v4/docs/API.md#get-idmeta).
+	//
+	// It returns the fully qualified id of the entity.
+	Meta(id *charm.URL, result interface{}) (*charm.URL, error)
+
+	// ServerURL returns the charm store URL used by the client.
+	ServerURL() string
+
+	// DisableStats disables incrementing download stats when retrieving
+	// archives from the charm store.
+	DisableStats()
+
+	// SetHTTPHeader sets custom HTTP headers that will be sent to the
+	// charm store on each request.
+	SetHTTPHeader(header http.Header)
+}
+
 // CharmStore is a repository Interface that provides access to the public Juju
 // charm store.
 type CharmStore struct {
-	client *csclient.Client
+	client CharmStoreClient
 }
 
 var _ Interface = (*CharmStore)(nil)
